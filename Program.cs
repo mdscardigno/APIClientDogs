56289f1135ed25e2d9796cfd1a728ada8cdc04ff
Milagro Scardigno <mdscardigno@gmail.com>
2023-02-20 22:13:01 -0500

    I get message and status!

--- Program.cs
+++ Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ConsoleTables;
 namespace APIClientDogs
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class TestJson
     {
-      Console.WriteLine("Welcome to C#");
+
     }
-  }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var keepGoing = true;
+            while (keepGoing)
+            {
+                Console.WriteLine("Welcome to API Client For Dogs!");
+                var dogbreed = PromptForInput("What breed of dog would you like to see?");
+
+                await GetBreeds(dogbreed);
+
+                var keepGoingInput = PromptForInput("Would you like to keep going? (y/n)").ToUpper();
+                if (keepGoingInput == "N")
+                {
+                    keepGoing = false;
+                }
+            }
+
+            static string PromptForInput(string prompt)
+            {
+                Console.WriteLine(prompt);
+                return Console.ReadLine();
+            }
+
+            static async Task GetBreeds(string dogbreed)
+            {
+                var client = new HttpClient();
+                var url = $"https://dog.ceo/api/breed/{dogbreed}/images";
+                var responseBodyAsStream = await client.GetStreamAsync(url);
+                Response messageObj = await JsonSerializer.DeserializeAsync<Response>(responseBodyAsStream);
+                var responseObj = messageObj.Message;
+                var status = messageObj.Status;
+                Console.WriteLine($"Status: {status}");
+                Console.WriteLine($"Message: {responseObj[0]}");
+                // //Table Formatting
+                // var table = new ConsoleTable("Message", "Status");
+                // table.AddRow(messageObj);
+                // table.Write();
+
+            }
+        }
+    }
 }
---
+++ Response.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace APIClientDogs
+{
+    internal class Response
+    {
+        [JsonPropertyName("message")]
+        public string[] Message { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+    }
+}
